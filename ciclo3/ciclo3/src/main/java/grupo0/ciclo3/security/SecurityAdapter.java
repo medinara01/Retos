56b1a7611937c779998645e2b6401ccd389197ea
medinara01 <rafael.medina.mt@correo.usa.edu.co>
2021-10-26 17:59:01 -0500

    autenticacion

---
+++ ciclo3/ciclo3/src/main/java/grupo0/ciclo3/security/SecurityAdapter.java
@@ -0,0 +1,32 @@
+package grupo0.ciclo3.security;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+
+    // ...
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	// @formatter:off
+        http
+            .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+            .oauth2Login();
+        // @formatter:on
+    }
+
+}
